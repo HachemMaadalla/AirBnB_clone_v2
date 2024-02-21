c01fa3b55442a8fcadaabb61ee617e713bd835ff
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-02-21 02:15:45 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-"""Defines the User class."""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
     """Represents a user for a MySQL database.
